a0d52e04569027b70d6f09d6ef35d68a24758b95
lungelimb <lungelimb@rocksolid.local>
2018-09-15 22:46:24 +0300

    local packages update

---
+++ elpa/company-childframe-20180705.546/company-childframe-autoloads.el
@@ -0,0 +1,36 @@
+;;; company-childframe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "company-childframe" "company-childframe.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from company-childframe.el
+
+(defvar company-childframe-mode nil "\
+Non-nil if Company-Childframe mode is enabled.
+See the `company-childframe-mode' command
+for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `company-childframe-mode'.")
+
+(custom-autoload 'company-childframe-mode "company-childframe" nil)
+
+(autoload 'company-childframe-mode "company-childframe" "\
+Company-childframe minor mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-childframe-autoloads.el ends here
